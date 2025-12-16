ebd8e1c7edb8f4d3366c1b9efddd8ac51319bb60
Hassan Erfani <hassan.erfani@proton.me>
2025-12-16 01:03:04 +0100

    SB18: Change chroma connection to http

--- backend/app/services/chroma_service.py
+++ backend/app/services/chroma_service.py
@@ -1,9 +1,11 @@
+import os
 import chromadb
 
 class ChromaService:
     def __init__(self):
-        self.client = chromadb.PersistentClient(
-            path="./chroma_data"
+        self.client = chromadb.HttpClient(
+            host=os.getenv("CHROMA_HOST", "localhost"),
+            port="8100"
         )
 
         self.collection = self.client.get_or_create_collection(
